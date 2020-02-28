340d60c32faf16f7eefa7eef04a4a462ff07fff4
Luke Phillips <lukephillips9216@gmail.com>
2020-02-28 22:15:35 +1100

    Base Activity and Fragment initalized.

--- app/src/main/java/com/cmcmarkets/android/exercise/base/BaseActivity.kt
+++ app/src/main/java/com/cmcmarkets/android/exercise/base/BaseActivity.kt
@@ -1,14 +1,37 @@
 package com.cmcmarkets.android.exercise.base
 
 import android.os.Bundle
+import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.cmcmarkets.android.exercise.R
+import dagger.android.support.DaggerAppCompatActivity
 
-class BaseActivity : AppCompatActivity() {
+abstract class BaseActivity : DaggerAppCompatActivity() {
+
+    /**
+     *  Set the Activity XML layout
+     */
+    @LayoutRes
+    internal abstract fun obtainLayoutResId(): Int
+
+    /**
+     * initiate Event
+     */
+    protected abstract fun initEvent()
+
+    /**
+     * initiate  View
+     */
+    protected abstract fun initView()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        // Set XML layout
+        setContentView(obtainLayoutResId())
+
+        initEvent()
+        initView()
     }
 
     private fun setFragment(fragment: Fragment) {
@@ -16,4 +39,6 @@ class BaseActivity : AppCompatActivity() {
                 .replace(R.id.fragment_container, fragment)
                 .commit()
     }
+
+
 }
