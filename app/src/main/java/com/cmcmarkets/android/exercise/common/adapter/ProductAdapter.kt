6ddd776e685ad71e811b52603d806c45520b1458
Luke Phillips <lukephillips9216@gmail.com>
2020-03-01 17:30:04 +1100

    Base setup complete

---
+++ app/src/main/java/com/cmcmarkets/android/exercise/common/adapter/ProductAdapter.kt
@@ -0,0 +1,28 @@
+package com.cmcmarkets.android.exercise.common.adapter
+
+import android.content.Context
+import android.view.View
+import com.cmcmarkets.android.data.gateway.entity.ProductDTO
+import com.cmcmarkets.android.exercise.base.BaseRecyclerViewAdapter
+import com.cmcmarkets.android.exercise.common.card.ProductCard
+import javax.inject.Inject
+
+class ProductAdapter @Inject constructor(productList: MutableList<ProductDTO>) :
+        BaseRecyclerViewAdapter<ProductDTO>(productList) {
+
+    var onProductClicked: ((ProductDTO) -> Unit)? = null
+
+    override fun getCardView(context: Context, viewType: Int): View {
+        return ProductCard(context)
+    }
+
+    override fun bindData(itemView: View, data: ProductDTO, position: Int) {
+        if(itemView is ProductCard) {
+            itemView.setProduct(data)
+        }
+    }
+
+    override fun getItemClickListener(): ((ProductDTO) -> Unit)? {
+        return onProductClicked
+    }
+}
